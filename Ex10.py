bbf2f111a701531486007f4efa5e876daacf9913
jriudavets20723@iesjoanramis.org <jriudavets20723@iesjoanramis.org>
2023-10-24 13:22:41 +0200

    ex10

---
+++ Ex10.py
@@ -0,0 +1,12 @@
+#Definicio de la funcio
+def major(a):
+    if x>18:
+        print("Es major d'edat")
+    elif x<18:
+        print("Es menor d'edat")
+    else: 
+        print ("te 18 anys")
+
+#Programa principal
+x = int(input("Introdueix la teva edat: "))
+major (x)   
---
+++ Ex9.py
@@ -0,0 +1,3 @@
+nom = input("Introdueix el seu nom: ")
+cognom = input("Introdueix el cognom: ")
+print("hola, {} {}". format(nom, cognom))
